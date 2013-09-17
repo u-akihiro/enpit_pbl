80d5e0444e54296ec67b0009022d396620577920
vagrant <vagrant@precise>
2013-09-17 04:01:25 +0000

    initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnpitPbl::Application.config.secret_key_base = 'a849f68e2d6bf2038e131187bb576b2ac8d9c5a5c9a67e212006fccc7fba877e5245e3b3f8ac9bd366ccd749f83773c356b31efb3361676cfa43d264085e6a6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EnpitPbl::Application.config.session_store :cookie_store, key: '_enpit_pbl_session'
